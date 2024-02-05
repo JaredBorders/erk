b362df5463f854d747f877d3b032bf9ea9284537
JaredBorders <jaredborders@pm.me>
2024-02-05 16:28:30 -0500

    👷🏻‍♂️ refactor -> get_hashes

--- library/erk.ml
+++ library/erk.ml
@@ -1,7 +1,7 @@
 open! Base
 
 type merkle_tree =
-  | Leaf of string
+  | Empty
   | Node of string * merkle_tree * merkle_tree
 [@@deriving compare]
 
@@ -21,60 +21,85 @@ let%test "Testing concatenating and hashing two strings..." =
     "7735464dca434e1d3d7198ccb7a99daf32f0ba13e1182de2eab8daff487b061b"
 ;;
 
-let validate_list_length lst =
-  let len = List.length lst in
-  if len = 0 || len = 1
-  then false
-  else (
-    let rec f acc =
-      match acc with
-      | 1 -> true
-      | x -> if x % 2 = 0 then f (x / 2) else false
-    in
-    f len)
-;;
-
-let%test "Testing validating merkle tree leaves length..." =
-  validate_list_length [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ]
-  && (not @@ validate_list_length [ "a"; "b"; "c"; "d"; "e"; "f" ])
-  && (not @@ validate_list_length [ "a"; "b"; "c" ])
-  && (not @@ validate_list_length [ "a" ])
-  && (not @@ validate_list_length [])
-;;
-
-let build_parents leaves =
-  let rec build_aux leaves acc =
-    match leaves with
-    | [] -> acc
-    | x :: y :: rest -> build_aux rest (concat_hash x y :: acc)
-    | _ -> failwith "build_parents: odd number of leaves"
+let get_hashes leaves hash fill =
+  let rec f' acc = function
+    | [] -> List.rev acc
+    | x :: y :: rest -> f' (hash x y :: acc) rest
+    | x :: _ -> List.rev (hash x fill :: acc)
   in
-  build_aux leaves [] |> List.rev
-;;
-
-let%test "Testing building parents list..." =
-  [%compare.equal: string list]
-    (build_parents [ "a"; "b"; "c"; "d" ])
-    [ concat_hash "a" "b"; concat_hash "c" "d" ]
-  && [%compare.equal: string list] (build_parents [ "a"; "b" ]) [ concat_hash "a" "b" ]
-;;
-
-let aggregate_parents (leaves : string list) : string list list =
-  let rec aggregate_parents_aux (leaves : string list) (acc : string list list) =
-    match leaves with
+  let rec f'' (acc : 'a list) = function
     | [] | [ _ ] -> acc
-    | non_empty_list ->
-      let parent_list = build_parents non_empty_list in
-      aggregate_parents_aux parent_list (parent_list :: acc)
+    | lst ->
+      let result = f' [] lst in
+      f'' (result @ acc) result
   in
-  aggregate_parents_aux leaves [ leaves ]
+  (if List.length leaves = 1
+   then f'' [ hash (List.hd_exn leaves) fill ] leaves
+   else f'' [] leaves)
+  @ leaves
 ;;
 
-let%test "Testing aggregating parent lists..." =
-  [%compare.equal: string list list]
-    (aggregate_parents [ "a"; "b"; "c"; "d" ])
-    [ [ concat_hash (concat_hash "a" "b") (concat_hash "c" "d") ]
-    ; [ concat_hash "a" "b"; concat_hash "c" "d" ]
-    ; [ "a"; "b"; "c"; "d" ]
-    ]
+let print_hashes lst = List.iter lst ~f:(fun x -> Stdio.print_endline x)
+
+let%test "Testing get_hashes..." =
+  [%compare.equal: string list] (get_hashes [] concat_hash "") []
+  && [%compare.equal: string list]
+       (get_hashes [ "a" ] concat_hash "")
+       [ concat_hash "a" ""; "a" ]
+  && [%compare.equal: string list]
+       (get_hashes [ "a"; "b" ] concat_hash "")
+       [ concat_hash "a" "b"; "a"; "b" ]
+  && [%compare.equal: string list]
+       (get_hashes [ "a"; "b"; "c" ] concat_hash "")
+       [ concat_hash (concat_hash "a" "b") (concat_hash "c" "")
+       ; concat_hash "a" "b"
+       ; concat_hash "c" ""
+       ; "a"
+       ; "b"
+       ; "c"
+       ]
+  && [%compare.equal: string list]
+       (get_hashes [ "a"; "b"; "c"; "d" ] concat_hash "")
+       [ concat_hash (concat_hash "a" "b") (concat_hash "c" "d")
+       ; concat_hash "a" "b"
+       ; concat_hash "c" "d"
+       ; "a"
+       ; "b"
+       ; "c"
+       ; "d"
+       ]
+  && [%compare.equal: string list]
+       (get_hashes [ "a"; "b"; "c"; "d"; "e"; "f" ] (fun x y -> x ^ y) "?")
+       [ "abcdef?"; "abcd"; "ef?"; "ab"; "cd"; "ef"; "a"; "b"; "c"; "d"; "e"; "f" ]
+  && [%compare.equal: int list]
+       (get_hashes [ 1; 2; 3; 4 ] (fun x y -> x * y * 10) 0)
+       [ 24000; 20; 120; 1; 2; 3; 4 ]
 ;;
+
+(* let merkle_tree_from_list leaves =
+   match validate_list_length leaves with
+   | false -> failwith "merkle_tree_from_list: invalid list length"
+   | true -> Empty
+   ;;
+
+   let example_array_of_leaves = [ "a"; "b"; "c"; "d" ]
+
+   let example_merkle_tree =
+   let leaf1 = List.nth_exn example_array_of_leaves 0 in
+   let leaf2 = List.nth_exn example_array_of_leaves 1 in
+   let leaf3 = List.nth_exn example_array_of_leaves 2 in
+   let leaf4 = List.nth_exn example_array_of_leaves 3 in
+   let digest_1 = concat_hash leaf1 leaf2 in
+   let digest_2 = concat_hash leaf3 leaf4 in
+   let root = concat_hash digest_1 digest_2 in
+   Node
+   ( root
+   , Node (digest_1, Node (leaf1, Empty, Empty), Node (leaf2, Empty, Empty))
+   , Node (digest_2, Node (leaf3, Empty, Empty), Node (leaf4, Empty, Empty)) )
+   ;;
+
+   let%test "Testing merkle_tree_from_list..." =
+   [%compare.equal: merkle_tree]
+   example_merkle_tree
+   (merkle_tree_from_list example_array_of_leaves)
+   ;; *)
