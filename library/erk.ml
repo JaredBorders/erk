fa75d4667bdea897a91779d348dd23c123c901b9
JaredBorders <jaredborders@pm.me>
2024-02-04 22:04:42 -0500

    👷🏻‍♂️ impl aggregate parents

--- library/erk.ml
+++ library/erk.ml
@@ -15,7 +15,12 @@ let%test "Testing hashing a string..." =
 
 let concat_hash a b = hash_string (a ^ b)
 
-(* List of leaves must be non-empty and *)
+let%test "Testing concatenating and hashing two strings..." =
+  String.equal
+    (concat_hash "potato" "tomato")
+    "7735464dca434e1d3d7198ccb7a99daf32f0ba13e1182de2eab8daff487b061b"
+;;
+
 let validate_list_length lst =
   let len = List.length lst in
   if len = 0 || len = 1
@@ -29,7 +34,7 @@ let validate_list_length lst =
     f len)
 ;;
 
-let%test "Testing validate_list_length..." =
+let%test "Testing validating merkle tree leaves length..." =
   validate_list_length [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ]
   && (not @@ validate_list_length [ "a"; "b"; "c"; "d"; "e"; "f" ])
   && (not @@ validate_list_length [ "a"; "b"; "c" ])
@@ -40,15 +45,36 @@ let%test "Testing validate_list_length..." =
 let build_parents leaves =
   let rec build_aux leaves acc =
     match leaves with
-    | [] -> List.rev acc
+    | [] -> acc
     | x :: y :: rest -> build_aux rest (concat_hash x y :: acc)
     | _ -> failwith "build_parents: odd number of leaves"
   in
-  build_aux leaves []
+  build_aux leaves [] |> List.rev
 ;;
 
-let%test "Testing build_parents..." =
+let%test "Testing building parents list..." =
   [%compare.equal: string list]
     (build_parents [ "a"; "b"; "c"; "d" ])
-    [ hash_string @@ "a" ^ "b"; hash_string @@ "c" ^ "d" ]
+    [ concat_hash "a" "b"; concat_hash "c" "d" ]
+  && [%compare.equal: string list] (build_parents [ "a"; "b" ]) [ concat_hash "a" "b" ]
+;;
+
+let aggregate_parents (leaves : string list) : string list list =
+  let rec aggregate_parents_aux (leaves : string list) (acc : string list list) =
+    match leaves with
+    | [] | [ _ ] -> acc
+    | non_empty_list ->
+      let parent_list = build_parents non_empty_list in
+      aggregate_parents_aux parent_list (parent_list :: acc)
+  in
+  aggregate_parents_aux leaves [ leaves ]
+;;
+
+let%test "Testing aggregating parent lists..." =
+  [%compare.equal: string list list]
+    (aggregate_parents [ "a"; "b"; "c"; "d" ])
+    [ [ concat_hash (concat_hash "a" "b") (concat_hash "c" "d") ]
+    ; [ concat_hash "a" "b"; concat_hash "c" "d" ]
+    ; [ "a"; "b"; "c"; "d" ]
+    ]
 ;;
